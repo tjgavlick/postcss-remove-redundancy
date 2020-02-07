d28f8d04d24e9a01173f307f2314d76617071019
tjgavlick <8879532+tjgavlick@users.noreply.github.com>
2020-02-07 10:43:27 -0700

    Adjust documentation, add helper function to gather props

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ const selectorParser = require('postcss-selector-parser');
 
 module.exports = postcss.plugin('postcss-remove-redundancy', function () {
   return function (root) {
+
+    // iterating through all the rules in a stylesheet, we're going to look
+    // ahead and see if their declarations are rendered redundant by something
+    // further down the cascade
     root.walkRules(function walkRulesOuter(originRule) {
+
+      // store this rule's attributes for quick access later
       const originSelectors = generateSelectorSet(originRule.selector);
       const originStart = originRule.source.start;
-      const originProps = Object.create(null);
+      const originProps = gatherProps(originRule);
 
-      // populate object of this node's declarations for quick lookup
-      originRule.walkDecls(function gatherOriginProps(decl) {
-        originProps[decl.prop] = Object.create(null);
-        originProps[decl.prop].value = decl.value;
-        originProps[decl.prop].important = decl.important;
-      });
-
-      // if rule is empty, kill it and move on
+      // if this rule is empty, kill it and move on
       if (Object.keys(originProps).length === 0) {
         originRule.remove();
         return;
       }
 
       root.walkRules(function walkRulesInner(newRule) {
-        // do not process until we're past our origin point in the stylesheet
+        // do not process until we're past our origin rule
         const newStart = newRule.source.start;
         if (newStart.line < originStart.line ||
             (newStart.line === originStart.line &&
@@ -30,7 +29,9 @@ module.exports = postcss.plugin('postcss-remove-redundancy', function () {
           return;
         }
 
+        // store this rule's attributes for faster comparison operations
         const newSelectors = generateSelectorSet(newRule.selector);
+        const newProps = gatherProps(newRule);
 
         // if the origin is a superset of the new, check for forward redundancy
         // in the importance cascade
@@ -78,6 +79,27 @@ module.exports = postcss.plugin('postcss-remove-redundancy', function () {
 
 
 /*
+ * Store a rule's props in a dictionary structure
+ *
+ * Args:
+ *   rule (Rule): a PostCSS Rule object
+ *
+ * Returns:
+ *   An object whose keys are css property names and whose values are objects
+ *     containing the attributes of that property
+ */
+function gatherProps(rule) {
+  const result = Object.create(null);
+  rule.walkDecls(function gatherProp(decl) {
+    result[decl.prop] = Object.create(null);
+    result[decl.prop].value = decl.value;
+    result[decl.prop].important = decl.important;
+  });
+  return result;
+}
+
+
+/*
  * Generate a set of normalized component selectors
  *
  * Each separate selector (that is, selectors separated by a comma) in the
@@ -109,6 +131,8 @@ function generateSelectorSet(selector) {
  */
 function isSuperset(superset, subset) {
   // empty sets do not apply to this question
+  // I know, technically, any non-empty set contains the empty set. But
+  // practicelly, that just muddies the waters here
   if (superset.size === 0 || subset.size === 0) {
     return false;
   }
@@ -118,4 +142,42 @@ function isSuperset(superset, subset) {
     }
   }
   return true;
+}
+
+
+/*
+ * Perform a union of two sets
+ *
+ * Args:
+ *   a, b (Set): the two Sets to union
+ *
+ * Returns:
+ *   A new Set containing the elements of a ∪ b
+ */
+function union(a, b) {
+  const result = new Set(a);
+  for (let el of b) {
+    result.add(el);
+  }
+  return result;
+}
+
+
+/*
+ * Perform an intersection of two sets
+ *
+ * Args:
+ *   a, b (Set): the two Sets to intersect
+ *
+ * Returns:
+ *   A new Set containing the elements of a ∩ b
+ */
+function intersection(a, b) {
+  const result = new Set();
+  for (let el of a) {
+    if (b.has(el)) {
+      result.add(el);
+    }
+  }
+  return result;
 }
